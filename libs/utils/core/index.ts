bd92075f8f46854c6ea77abfa6c97db87aaef2bb
Danil Mosolov <koti4ch@gmail.com>
2023-07-08 15:10:58 +0500

    libs(utils): create a utils lib for front.web

---
+++ libs/utils/core/index.ts
@@ -0,0 +1,8 @@
+export * from './core';
+export * from './function/core';
+
+export { debounce } from './function/debounce';
+export { assoc } from './object/assoc';
+export { objectFromArray } from './object/create';
+export { exclude } from './array/exclude';
+export { isEmpty } from './is-empty';
