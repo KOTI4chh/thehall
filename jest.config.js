bb3345d21865ff3de70ad5aaac8696beb506b646
Danil Mosolov <koti4ch@gmail.com>
2023-07-08 15:03:50 +0500

    config(app): create a config app

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const { createJestProjectConfig } = require('@libs/config/jest');
+
+module.exports = {
+	projects: [
+		createJestProjectConfig({
+			displayName: '@apps/client-web',
+			pathsRoot: 'apps/client-web',
+			tsConfig: require.resolve('./apps/client-web/tsconfig.node.json'),
+			targets: ['apps/client-web'],
+		}),
+		createJestProjectConfig({
+			displayName: '@libs/<*>',
+			targets: ['libs/ui', 'libs/utils'],
+			tsConfig: require.resolve('./tsconfig.node.json'),
+		}),
+	],
+};
